ad70bd4b87d3be53cfbf0f6be41d072d9e374d6c
Erick Pham <erick.pham.dev@gmail.com>
2022-09-01 15:02:49 +0700

    fix bug

--- app/authSlice.ts
+++ app/authSlice.ts
@@ -23,14 +23,14 @@ export const authSlice = createSlice({
     },
 
     // Special reducer for hydrating the state. Special case for next-redux-wrapper
-    extraReducers: {
-      [HYDRATE]: (state, action) => {
-        return {
-          ...state,
-          ...action.payload.auth,
-        };
-      },
-    },
+    // extraReducers: {
+    //   [HYDRATE]: (state: any, action: { payload: { auth: any } }) => {
+    //     return {
+    //       ...state,
+    //       ...action.payload.auth,
+    //     };
+    //   },
+    // },
   },
 });
 
--- app/rootSlice.ts
+++ app/rootSlice.ts
@@ -37,14 +37,14 @@ export const rootSlice = createSlice({
     },
 
     // Special reducer for hydrating the state. Special case for next-redux-wrapper
-    extraReducers: {
-      [HYDRATE]: (state, action) => {
-        return {
-          ...state,
-          ...action.payload.auth,
-        };
-      },
-    },
+    // extraReducers: {
+    //   [HYDRATE]: (state, action) => {
+    //     return {
+    //       ...state,
+    //       ...action.payload.auth,
+    //     };
+    //   },
+    // },
   },
 });
 
