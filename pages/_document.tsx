019d9489beb8ec5363bfd1d20f3dfcf85d9b433e
Erick Pham <erick.pham.dev@gmail.com>
2022-09-11 14:59:27 +0700

    add fb mess

---
+++ pages/_document.tsx
@@ -0,0 +1,45 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <Script id="show-banner" strategy="beforeInteractive">
+          {`
+              <script>
+              var chatbox = document.getElementById('fb-customer-chat');
+              chatbox.setAttribute("page_id", "1528767773852708");
+              chatbox.setAttribute("attribution", "biz_inbox");
+            </script>
+        
+            <!-- Your SDK code -->
+            <script>
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v14.0'
+                });
+              };
+        
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+            </script>
+          `}
+        </Script>
+      </Head>
+      <body>
+        <div id="fb-root"></div>
+
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
