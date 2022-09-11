fc1d77549abe3e4c0d3c1da4e699996249f9861d
Erick Pham <erick.pham.dev@gmail.com>
2022-09-11 15:08:37 +0700

    fix bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,7 @@ export default function Document() {
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) return;
                 js = d.createElement(s); js.id = id;
+                js.crossorigin ="anonymous"
                 js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
                 fjs.parentNode.insertBefore(js, fjs);
               }(document, 'script', 'facebook-jssdk'));
