ee1603b5e70a3543e4019d6bb59648ec11c9b3c3
Erick Pham <erick.pham.dev@gmail.com>
2022-09-11 16:36:26 +0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,40 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <Script id="fb-div">
+        <script
+          data-partytown-config
+          dangerouslySetInnerHTML={{
+            __html: `
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "1528767773852708");
+            chatbox.setAttribute("attribution", "biz_inbox");
+            `,
+          }}
+        />
+        <script
+          data-partytown-config
+          crossOrigin="anonymous"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v14.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.crossorigin = 'anonymous'
+              js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            `,
+          }}
+        />
+        {/* <Script id="fb-div">
           {`
               var chatbox = document.getElementById('fb-customer-chat');
               chatbox.setAttribute("page_id", "1528767773852708");
@@ -28,10 +61,10 @@ export default function Document() {
                 js = d.createElement(s); js.id = id;
                 js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
                 fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));
+              }(document, 'script', 'facebook-jssdk', 'crossorigin="anonymous"'));
         
           `}
-        </Script>
+        </Script> */}
       </Head>
       <body>
         <div id="fb-root"></div>
