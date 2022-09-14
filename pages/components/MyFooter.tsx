32207af7d2452f332bbd895e276ebb60bff5687f
Erick Pham <erick.pham.dev@gmail.com>
2022-09-14 09:45:50 +0700

    fix text

--- pages/components/MyFooter.tsx
+++ pages/components/MyFooter.tsx
@@ -24,7 +24,7 @@ export const MyFooter = () => {
               Bảo mật
             </Typography>
             <Typography color={"white"}>
-              Cảm thấy tự tin mỗi khi bạn giao dịch với chúng tôi
+              Cảm thấy tự tin mỗi khi bạn giao dịch với chúng tôi.
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
@@ -43,7 +43,7 @@ export const MyFooter = () => {
             <Typography color={"white"}>
               Đội ngũ hỗ trợ Khách hàng tận tâm của chúng tôi luôn sẵn sàng trợ
               giúp mọi thắc mắc về đơn đặt hàng của bạn và cung cấp hỗ trợ sau
-              bán hàng
+              bán hàng.
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
@@ -60,7 +60,7 @@ export const MyFooter = () => {
               Giá tốt nhất
             </Typography>
             <Typography color={"white"}>
-              Gameshop247 cung cấp giá cả tốt nhất cho người mua,
+              Gameshop247 cung cấp giá cả tốt nhất cho người mua.
             </Typography>
           </Grid>
         </Grid>
