548294bfb4fbf59bf2ebf11288858ae406089f28
Erick Pham <erick.pham.dev@gmail.com>
2023-01-03 22:00:46 +0700

    add file missing

---
+++ components/ProductDetail.tsx
@@ -0,0 +1,74 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  Typography,
+  Stack,
+  Divider,
+  Chip,
+  CardMedia,
+} from "@mui/material";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import PriceCheckIcon from "@mui/icons-material/PriceCheck";
+import SellIcon from "@mui/icons-material/Sell";
+import numeral from "numeral";
+import { ProductItemType } from "./Types";
+import {
+  PRODUCT_ITEM_STATUS,
+  PRODUCT_ITEM_STATUS_TEXT_VI,
+} from "@common/constants";
+
+const ProductDetail = ({
+  productItem,
+}: {
+  productItem: ProductItemType | undefined;
+}) => {
+  const isItemSold = productItem?.status === PRODUCT_ITEM_STATUS.SOLD;
+  return (
+    <>
+      <Typography variant="h5" fontWeight={800}>
+        {productItem?.name}
+      </Typography>
+      <Typography variant="caption">
+        {`#${productItem?.referenceNumber || ""}`}
+      </Typography>
+      <Divider></Divider>
+      <Stack direction="row" alignItems="center" gap={1}>
+        <SellIcon />
+        {productItem && (
+          <Chip
+            label={
+              productItem?.status
+                ? PRODUCT_ITEM_STATUS_TEXT_VI[productItem.status]
+                : ""
+            }
+            color={isItemSold ? "warning" : "primary"}
+          />
+        )}
+      </Stack>
+      <Stack direction="row" alignItems="center" gap={1}>
+        <PriceCheckIcon />
+        <Typography variant="h6" color="text.primaryRed" fontWeight={800}>
+          {`${numeral(productItem?.price).format("0,0")} ${
+            productItem?.currency || ""
+          }`}
+        </Typography>
+      </Stack>
+      <Stack direction="row" alignItems="center" gap={1}>
+        <AssignmentIcon />
+        <Typography variant="subtitle1" color="text.primary">
+          Mô tả sản phẩm
+        </Typography>
+      </Stack>
+      <Typography variant="body1" color="text.primary">
+        {productItem?.description}
+      </Typography>
+      <CardMedia component="img" image={productItem?.thumbnail} alt="alt" />
+    </>
+  );
+};
+
+export default ProductDetail;
---
+++ components/Types.ts
@@ -0,0 +1,10 @@
+export type ProductItemType = {
+  id: string;
+  name: string;
+  referenceNumber: string;
+  price: number | null;
+  currency: string;
+  description: string;
+  thumbnail: string;
+  status: string;
+};
