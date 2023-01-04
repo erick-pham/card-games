6fccd6cbe02a14267d677760e2b5067309892102
Erick Pham <erick.pham.dev@gmail.com>
2023-01-04 14:49:15 +0700

    apply darkmode

--- app/rootSlice.ts
+++ app/rootSlice.ts
@@ -4,6 +4,7 @@ import { HYDRATE } from "next-redux-wrapper";
 import { AlertColor } from "@mui/material/Alert";
 // Type for our state
 export interface RootState {
+  themeMode: "light" | "dark";
   authState: boolean;
   loading: boolean;
   loadingMessage: {
@@ -23,6 +24,7 @@ export interface RootState {
 
 // Initial state
 const initialState: RootState = {
+  themeMode: "light",
   authState: false,
   loading: false,
   loadingMessage: {
@@ -56,6 +58,9 @@ export const rootSlice = createSlice({
     setDialogImageState(state, action) {
       state.dialogImage = action.payload;
     },
+    setThemeModeState(state, action) {
+      state.themeMode = action.payload;
+    },
     // Special reducer for hydrating the state. Special case for next-redux-wrapper
     // extraReducers: {
     //   [HYDRATE]: (state, action) => {
@@ -68,8 +73,12 @@ export const rootSlice = createSlice({
   },
 });
 
-export const { setErrorState, setLoadingState, setDialogImageState } =
-  rootSlice.actions;
+export const {
+  setErrorState,
+  setLoadingState,
+  setDialogImageState,
+  setThemeModeState,
+} = rootSlice.actions;
 
 export const selectErrorState = (state: AppState) => state.root.error;
 export const selectDialogImageState = (state: AppState) =>
@@ -77,5 +86,5 @@ export const selectDialogImageState = (state: AppState) =>
 export const selectLoadingState = (state: AppState) => state.root.loading;
 export const selectLoadingMessageState = (state: AppState) =>
   state.root.loadingMessage;
-
+export const selectThemeModeState = (state: AppState) => state.root.themeMode;
 export default rootSlice.reducer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,8 @@ import {
   setErrorState,
   selectLoadingState,
   selectLoadingMessageState,
-} from "../app/rootSlice";
+  selectThemeModeState,
+} from "app/rootSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { wrapper } from "app/store";
 import LoadingDialog from "components/LoadingDialog";
@@ -22,7 +23,6 @@ import { themeLight, themeDark } from "theme";
 import createEmotionCache from "theme/createEmotionCache";
 import { CacheProvider } from "@emotion/react";
 import Script from "next/script";
-import MyFooterSettings from "components/MyFooterSettings";
 const FACEBOOK_PAGE_ID = process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID;
 const ATTRIBUTION = process.env.NEXT_PUBLIC_FACEBOOK_ATTRIBUTION;
 const clientSideEmotionCache = createEmotionCache();
@@ -68,24 +68,24 @@ function MyApp({
   };
   const getLayout = Component?.getLayout ?? ((page: any) => page);
 
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  // const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  // const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   // useMemo(() => setIsDarkTheme(prefersDarkMode), [prefersDarkMode]);
 
   // The light theme is used by default
 
   // This function is triggered when the Switch component is toggled
-  const changeTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
-  };
-
+  // const changeTheme = () => {
+  //   setIsDarkTheme(!isDarkTheme);
+  // };
+  const themeMode = useSelector(selectThemeModeState);
   return (
     <div>
       <div id="fb-root"></div>
       <div id="fb-customer-chat" className="fb-customerchat"></div>
-      {process.env.FB_CHAT === "true" && (
+      {process.env.FB_CHAT === "true" ? (
         <Script
           id="fb-chat"
           strategy="afterInteractive"
@@ -111,6 +111,8 @@ function MyApp({
           `,
           }}
         />
+      ) : (
+        <></>
       )}
 
       <LoadingDialog
@@ -132,7 +134,7 @@ function MyApp({
         </Alert>
       </Snackbar>
       <CacheProvider value={emotionCache}>
-        <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+        <ThemeProvider theme={themeMode === "light" ? themeLight : themeDark}>
           <CssBaseline />
           <SessionProvider session={session}>
             {Component.auth ? (
@@ -143,10 +145,6 @@ function MyApp({
               getLayout(<Component {...pageProps} />)
             )}
           </SessionProvider>
-          <MyFooterSettings
-            darkModevalue={isDarkTheme}
-            onChangeDarkMode={changeTheme}
-          ></MyFooterSettings>
         </ThemeProvider>
       </CacheProvider>
     </div>
