57856049f2fbc427510cc4277a6d395e4b8f9db8
Erick Pham <erick.pham.dev@gmail.com>
2023-01-05 17:39:19 +0700

    clean code

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -1,64 +1,3 @@
-// import { useState, useEffect } from "react";
-
-// import { Editor, EditorState, convertFromRaw, convertToRaw } from "draft-js";
-// import "draft-js/dist/Draft.css";
-
-// import dynamic from "next/dynamic";
-// // import apiClient from '../api/api_client'
-
-// // const Editor = dynamic(
-// //   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
-// //   { ssr: false }
-// // );
-
-// import DOMPurify from "dompurify";
-
-// function MyEditor() {
-//   const [editorState, setEditorState] = useState(() =>
-//     EditorState.createEmpty()
-//   );
-//   const [convertedContent, setConvertedContent] = useState(null);
-//   // useEffect(() => {
-//   //   let html = convertToRaw(editorState.getCurrentContent());
-//   //   setConvertedContent(html);
-//   // }, [editorState]);
-
-//   console.log(convertedContent);
-
-//   const uploadImageCallBack = async (file) => {
-//     // const imgData = await apiClient.uploadInlineImageForArticle(file);
-//     return Promise.resolve({
-//       data: {
-//         link: "${process.env.NEXT_PUBLIC_API_URL}${imgData[0].formats.small.url}",
-//       },
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <Editor
-//         editorState={editorState}
-//         onEditorStateChange={setEditorState}
-//         toolbarClassName="toolbar-class"
-//         wrapperClassName="wrapper-class"
-//         editorClassName="editor-class"
-//       />
-//       <div
-//         className="preview"
-//         // dangerouslySetInnerHTML={createMarkup(convertedContent)}
-//       ></div>
-//     </div>
-//   );
-// }
-
-// function createMarkup(html) {
-//   return {
-//     __html: DOMPurify.sanitize(html),
-//   };
-// }
-
-// export default MyEditor;
-
 import dynamic from "next/dynamic";
 import React, { useState } from "react";
 
@@ -67,13 +6,14 @@ const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const MyRichEditor = () => {
   const [value, setValue] = useState("");
+
   const modules = {
     toolbar: [
-      // [
-      //   {
-      //     font: Font.whitelist,
-      //   },
-      // ],
+      [
+        {
+          font: [],
+        },
+      ],
 
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
