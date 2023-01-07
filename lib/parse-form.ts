fdcc2e7ea60e0c4edf7ce25188cac4b959e67c70
Erick Pham <erick.pham.dev@gmail.com>
2023-01-07 20:55:53 +0700

    add formidable

---
+++ lib/parse-form.ts
@@ -0,0 +1,59 @@
+import formidable from "formidable";
+import type { NextApiRequest } from "next";
+import mime from "mime";
+import { join } from "path";
+import * as dateFn from "date-fns";
+import { mkdir, stat } from "fs/promises";
+export const FormidableError = formidable.errors.FormidableError;
+
+export const parseForm = async (
+  req: NextApiRequest
+): Promise<{
+  fields: formidable.Fields;
+  files: formidable.Files;
+  baseHost: string;
+}> => {
+  return new Promise(async (resolve, reject) => {
+    const baseHost = `uploads/${dateFn.format(Date.now(), "dd-MM-Y")}`;
+
+    const uploadDir = join(
+      process.env.ROOT_DIR || process.cwd(),
+      `/public/${baseHost}`
+    );
+    try {
+      await stat(uploadDir);
+    } catch (e: any) {
+      if (e.code === "ENOENT") {
+        await mkdir(uploadDir, { recursive: true });
+      } else {
+        console.error(e);
+        reject(e);
+        return;
+      }
+    }
+
+    const form = formidable({
+      multiples: true,
+      maxFiles: 2,
+      maxFileSize: 1024 * 1024, // 1mb
+      uploadDir,
+      filename: (_name, _ext, part) => {
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        const filename = `${part.name || "unknown"}-${uniqueSuffix}.${
+          mime.getExtension(part.mimetype || "") || "unknown"
+        }`;
+        return filename;
+      },
+      filter: (part) => {
+        return (
+          part.name === "file" && (part.mimetype?.includes("image") || false)
+        );
+      },
+    });
+
+    form.parse(req, function (err, fields, files) {
+      if (err) reject(err);
+      else resolve({ fields, files, baseHost });
+    });
+  });
+};
---
+++ pages/api/files.ts
@@ -0,0 +1,79 @@
+// import formidable from "formidable";
+// import fs from "fs";
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// const post = async (req, res) => {
+//   const form = new formidable.IncomingForm();
+//   form.parse(req, async function (err, fields, files) {
+//     await saveFile(files.file);
+//     return res.status(201).send("");
+//   });
+// };
+
+// const saveFile = async (file) => {
+//   const data = fs.readFileSync(file.path);
+//   fs.writeFileSync(`./public/${file.name}`, data);
+//   await fs.unlinkSync(file.path);
+//   return;
+// };
+
+// export default (req, res) => {
+//   req.method === "POST"
+//     ? post(req, res)
+//     : req.method === "PUT"
+//     ? console.log("PUT")
+//     : req.method === "DELETE"
+//     ? console.log("DELETE")
+//     : req.method === "GET"
+//     ? console.log("GET")
+//     : res.status(404).send("");
+// };
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { parseForm, FormidableError } from "lib/parse-form";
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<{
+    success?: boolean;
+    data: {
+      url: string | string[];
+    } | null;
+    error: string | null;
+  }>
+) => {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.status(405).json({
+      data: null,
+      error: "Method Not Allowed",
+    });
+    return;
+  }
+  const { fields, files, baseHost } = await parseForm(req);
+  console.log({ fields, files });
+  const file = files.file;
+  let url = Array.isArray(file)
+    ? file.map((f) => `${baseHost}/${f.newFilename}`)
+    : `${baseHost}/${file.newFilename}`;
+
+  res.status(200).json({
+    success: true,
+    data: {
+      url,
+    },
+    error: null,
+  });
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
