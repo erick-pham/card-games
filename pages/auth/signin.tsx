86cd38e5ae062937817614416f478ad7f503dfd5
Erick Pham <erick.pham.dev@gmail.com>
2023-01-11 11:43:19 +0700

    add facebook login

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -120,7 +120,6 @@ export default function SignIn({
               </Button>
 
               <Button
-                disabled
                 color="info"
                 fullWidth
                 startIcon={<FacebookIcon />}
