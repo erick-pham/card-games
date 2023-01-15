371c05797472a90cc598130dc025d134e6883074
Erick Pham <erick.pham.dev@gmail.com>
2023-01-15 18:38:01 +0700

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'developmenta'
+  disable: process.env.NODE_ENV === 'development'
 })
 
 module.exports = withPWA({
