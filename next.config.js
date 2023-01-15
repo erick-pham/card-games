b51c9c22159bdec48b6b19be6f9f440485f2dc9a
Erick Pham <erick.pham.dev@gmail.com>
2023-01-15 19:04:58 +0700

    update pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa')({
   dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
   disable: process.env.NODE_ENV === 'development'
 })
 
